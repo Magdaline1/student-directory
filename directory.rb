a9f0934360637e3810ce20ebb94f9043ed1f5cce
Magdaline <mamoakohene@gmail.com>
2022-11-04 10:42:06 +0000

    loading from command line

--- directory.rb
+++ directory.rb
@@ -3,12 +3,12 @@
 def input_students
     puts "Please enter the name of the student(s)"
     puts "To finish, just hit return twice"
-    name = gets.chomp
+    name = STDIN.gets.chomp
 
     while !name.empty? do
         @students << {name: name, cohort: :november}
         puts "Now we have #{@students.count} students"
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
     @students
 end
@@ -32,7 +32,7 @@ end
 def interactive_menu
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
@@ -78,8 +78,8 @@ def save_students
 end
 
 
-def load_students
-    file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
     file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
         @students << {name: name, cohort: cohort.to_sym}
@@ -88,6 +88,18 @@ def load_students
 end
 
 
+def try_load_students
+    filename = ARGV.first
+    return if filename.nil?
+    if File.exist?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.count} from #{filename}"
+    else
+        puts "Sorry, #{filename} doesn't exist."
+        exit
+    end
+end
 
 
+try_load_students
 interactive_menu
